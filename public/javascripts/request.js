541483242ed6044fc6ad7561897d3e7564a6b91b
Lucas Rocha <lucas.rocha@ecostage.com.br>
2019-11-19 13:21:54 -0300

    first commit

--- public/javascripts/request.js
+++ public/javascripts/request.js
@@ -1,5 +1,20 @@
-var request = function Request(url) {
-  fetch(`${url}`)
+var person = [];
+var dom = document.getElementById("root");
+
+const request = function Request() {
+  fetch("http://localhost:7007/json/fazenda.json")
     .then(req => req.json())
-    .then(json => console.log(json));
+    .then(json => {
+      person = json.data;
+    });
+  setTimeout(function() {
+    return person.map(function(item, index) {
+      var list = document.createElement("h1");
+      list.setAttribute("id", index);
+      dom.append(list);
+      list.innerHTML = item.name;
+    });
+  }, 100);
 };
+
+request();
